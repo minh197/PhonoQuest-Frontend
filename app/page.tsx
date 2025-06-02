5591928c784b9ce8a4e73fc302ecc693b8fb7048
Minh Nguyen <139167959+mnguyenKU@users.noreply.github.com>
2025-06-02 10:51:13 -0500

    first commit here

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    //comment here
+    <main className="flex min-h-screen items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600">PhonoQuest Frontend</h1>
+    </main>
+  );
+}
